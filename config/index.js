33fe3227bee2a7173d1322131b3d34634349eeb7
Yinxiong <languid.jeremy@gmail.com>
2017-05-29 16:33:19 +0800

    update package version

--- config/index.js
+++ config/index.js
@@ -6,10 +6,34 @@ const fs = require('fs');
 const _ = require('lodash');
 const os = require('os');
 
+const manifestBasePath = '';
+const manifestProjectPath = '';
+
+const ifaces = os.networkInterfaces();
+
+let ipList = [];
+Object.keys(ifaces).forEach(function (ifname) {
+    var alias = 0;
+
+    ifaces[ifname].forEach(function (iface) {
+        if ('IPv4' !== iface.family || iface.internal !== false) {
+            // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+            return;
+        }
+
+        if (alias >= 1) {
+            // this single interface has multiple ipv4 addresses
+            ipList.push(iface.address);
+        } else {
+            // this interface has only one ipv4 adress
+            ipList.push(iface.address);
+        }
+        ++alias;
+    });
+});
+
 const devPort = 9391;
-const platform = os.platform();
-const manifestBasePath = (platform == 'win32')? 'C:/Users/jabbar/IdeaProjects/': '/Users/jabbar/IdeaProjects/';
-const manifestProjectPath = 'wifipix-wificloud2.0/src/main/webapp/WEB-INF/views/common/inc/';
+const localIp = ipList.find(d=>d.indexOf('192.168') == 0);
 
 module.exports = {
     srcDir: './src',
@@ -33,8 +57,9 @@ module.exports = {
         productionSourceMap: false
     },
     dev: {
+        ip: localIp,
         port: devPort,
-        publicPath: `http://localhost:${devPort}/dist/js/`,
+        publicPath: `http://${localIp}:${devPort}/dist/js/`,
         proxyTable: {}
     },
 	manifest: 'rev-manifest.json',
--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 /**
  * Created by Yinxiong on 2016/3/11 0011.
  */
+
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 const clean = require('gulp-clean-css');
 const config = require('./config');
 const sourcemaps = require('gulp-sourcemaps');
@@ -11,97 +13,41 @@ const _ = require('lodash');
 const path = require('path');
 const fs = require('fs');
 const runSequence = require('run-sequence');
-const swig = require('gulp-swig');
 
 const cssPath = `${config.srcDir}/scss`;
 const jsPath = `${config.buildDir}/js`;
-const viewPath = `${config.srcDir}/views`;
 
-const swigDefaults = {
-    defaults: {
-        cache: false,
-        // varControls: ['<%', '%>'],
-        locals: {
-            version: +(new Date)
-        }
-    }
-};
-
-const devLocals = _.merge({}, swigDefaults, {
-    defaults: {
-        locals: {
-            staticPath: '/dist',
-            basePath: '/',
-            cssArray: readCss(),
-            jsObj: _.reduce(readJs(), (result, item)=>{
-                result[item.name] = item.value;
-                return result;
-            }, {})
-        }
-    }
-});
-
-const buildLocals = _.merge({}, swigDefaults, {
-    defaults: {
-        locals: {
-            staticPath: '/dist',
-            basePath: '/',
-            cssArray: readCss(true),
-            jsObj: _.reduce(readJs(true), (result, item)=>{
-                result[item.name] = item.value;
-                return result;
-            }, {})
-        }
-    }
-});
-
-function readCss(isProduction){
-    if(!isProduction){
-        let arr = [];
-        _.forEach(fs.readdirSync(cssPath), (f)=>{
-            let stat = fs.statSync(path.join(cssPath, f));
-            if(stat.isFile()){
-                let name = path.parse(f).name;
-                if(name.indexOf('_') != 0){
-                    arr.push({name: name, value: name})
-                }
-            }
-        });
-        return arr;
-    };
-
-    const manifestPath = path.join(config.distDir, config.manifest);
+function readCss(cssPath){
     let arr = [];
-    let data = fs.readFileSync(manifestPath);
-    const list = JSON.parse(data);
-    _.forEach(list, (value, key)=> {
-        let p = path.parse(key);
-        let varName = p.dir.replace('/', '_') + '_' + p.name.replace(/\./g, '_');
-        arr.push({name: varName, value: path.parse(value).name});
+    _.forEach(fs.readdirSync(cssPath), (f)=>{
+        let stat = fs.statSync(path.join(cssPath, f));
+        if(stat.isFile()){
+            let name = path.parse(f).name;
+            if(name.indexOf('_') != 0){
+                arr.push(`<c:set var="css_${name}" value="css/${name}.css" />`)
+            }
+        }
     });
-
     return arr;
-}
-
-function _getChunkNames(){
-    let names = _.map(config.commonChunks.filter((item)=>{
-        return _.has(item, 'names');
-    }), 'names');
-    let name = _.map(config.commonChunks.filter((item)=>{
-        return _.has(item, 'name');
-    }), 'name');
-    return _.uniq(_.flatten(names.concat(name)));
 }
 
 function readJs(isProduction){
     if(!isProduction){
-        return _.map(
-            _.uniq(_.flatten(_.keys(config.entry).concat(_getChunkNames()))),
-            (p)=>{
-                if(p){
-                    return {name: p, value: p};
-                }
+
+        let names = {};
+        _.forEach(config.commonChunks, (d, index)=>{
+            if(d.name){
+                names[d.name] = d.name;
+            } else if( d.names ){
+                _.map(d.names, d=>{
+                    names[d] = d
+                })
             }
+        });
+
+        return _.map(
+            Object.assign({}, config.entry, names),
+            (p, key)=>`<c:set var="js_${key}" value="js/${key}.js" />`
         )
     }
 
@@ -111,12 +57,9 @@ function readJs(isProduction){
         let stat = fs.statSync(path.join(jsPath, f));
         if(stat.isFile()){
             let {name, ext} = path.parse(f);
-            if(ext == '.js' && /[a-zA-Z0-9]+\.+\D+/.test(name)){
-                let realName = _.last(name.split('-')[0].split('.'));
-                arr.push({
-                    name: realName,
-                    value: name
-                });
+            if(ext == '.js' && /[a-zA-Z0-9]+\.\D+/.test(name)){
+                let realName = name.substring(name.indexOf('.')+1, name.length);
+                arr.push(`<c:set var="js_${realName}" value="js/${f}" />`)
             }
         }
     });
@@ -124,97 +67,83 @@ function readJs(isProduction){
     return arr;
 }
 
-gulp.task('swig', function () {
-    return gulp.src(`${viewPath}/*.html`)
-        .pipe(swig(devLocals))
-        .pipe(gulp.dest(`${config.distDir}`))
-});
-
-gulp.task('swig:build', function () {
-    return gulp.src(`${viewPath}/*.html`)
-        .pipe(swig(buildLocals))
-        .pipe(gulp.dest(`${config.distDir}`))
-});
-
 gulp.task('sass', function () {
     return gulp.src(config.srcDir + '/scss/*.scss')
         .pipe(sourcemaps.init())
-        .pipe(sass().on('error', sass.logError))
+        .pipe(sass({
+            outputStyle: 'compressed',
+        }).on('error', sass.logError))
+        .pipe(autoprefixer())
         .pipe(sourcemaps.write('./maps'))
         .pipe(gulp.dest(config.distDir + '/css/'))
-		.pipe(gulp.dest(config.buildDir + '/css/'))
-});
-
-gulp.task('clean:css', function(){
-    return gulp.src(config.srcDir + '/scss/*.scss')
-        .pipe(sourcemaps.init())
-        .pipe(sass().on('error', sass.logError))
-        .pipe(clean())
-        .pipe(gulp.dest(config.distDir + '/css/'))
+        .pipe(gulp.dest(config.buildDir + '/css/'))
 });
 
 gulp.task('watch', function () {
-	gulp.watch(`${config.srcDir}/scss/**/*.scss`, ['sass']);
-    gulp.watch(`${viewPath}/**/*.html`, ['swig']);
+    gulp.watch(config.srcDir+'/scss/**/*.scss', ['sass']);
 });
+
 
 //生成开发版静态依赖
 gulp.task('generateDevManifest', ()=>{
-	let content = ['<%@ page language="java" contentType="text/html;charset=utf-8" pageEncoding="utf-8" session="false"%>'];
-	content.push(`<c:set var="static_server_port" value="${config.dev.port}" />`);
-	content = content.concat(_.map(readCss(), name=>`<c:set var="css_${name.name}" value="css/${name.value}.css" />`));
-	content = content.concat(_.map(readJs(), (v)=>`<c:set var="js_${v.name}" value="js/${v.value}.js" />`));
-	content.push(`<c:set var="js_common" value="js/common.js" />`);
+    let content = ['<%@ page language="java" contentType="text/html;charset=utf-8" pageEncoding="utf-8" session="false"%>'];
+    content.push(`<c:set var="static_server_port" value="${config.dev.port}" />`);
+    content.push(`<c:set var="static_server_ip" value="${config.dev.ip}" />`);
+    content = content.concat(readCss(cssPath));
+    content = content.concat(readJs());
 
-	fs.writeFile(config.jspManifestDev, content.join('\n'), (err) => {
-		if (err) throw err;
-		console.log('Development Manifest OK');
-	});
+    fs.writeFile(config.jspManifestDev, content.join('\n'), (err) => {
+        if (err) throw err;
+        console.log('Development Manifest OK');
+    });
 });
 
 //生成生产版静态依赖
 gulp.task('generateProdManifest', ()=>{
-	console.log('> write manifest to _manifest_prod.jsp');
+    console.log('> write manifest to _manifest_prod.jsp');
 
-	const manifestPath = path.join(config.distDir, config.manifest);
-	let content = ['<%@ page language="java" contentType="text/html;charset=utf-8" pageEncoding="utf-8" session="false"%>'];
-	content.push(`<c:set var="static_server_port" value="${config.dev.port}" />`);
-
-    _.map(readCss(true), v=>{
-        content.push(`<c:set var="${v.name}" value="${v.value}.css" />`)
-    });
-
-    content = content.concat(_.map(readJs(true), (v)=>`<c:set var="js_${v.name}" value="js/${v.value}.js" />`));
-
-    fs.writeFile(config.jspManifestProd, content.join('\n'), (err) => {
+    const manifestPath = path.join(config.distDir, config.manifest);
+    let content = ['<%@ page language="java" contentType="text/html;charset=utf-8" pageEncoding="utf-8" session="false"%>'];
+    content.push(`<c:set var="static_server_port" value="${config.dev.port}" />`);
+    fs.readFile(manifestPath, (err, data)=>{
         if (err) throw err;
-        console.log('Production Manifest OK');
-    })
+        const list = JSON.parse(data);
+        _.forEach(list, (value, key)=>{
+            let p = path.parse(key);
+            let varName = p.dir.replace('/', '_') + '_' + p.name.replace(/\./g, '_');
+            content.push(`<c:set var="${varName}" value="${value}" />`)
+        });
+
+        content = content.concat(readJs(true));
+
+        fs.writeFile(config.jspManifestProd, content.join('\n'), (err) => {
+            if (err) throw err;
+            console.log('Production Manifest OK');
+        })
+    });
 });
 
-gulp.task('copy:js', ()=>{
-	console.log('copy js to dist');
-	
-	return gulp.src(`${config.buildDir}/js/*.js`)
-		.pipe(gulp.dest(`${config.distDir}/js/`));
-});
-
+//文件版本号添加
 gulp.task('rev', function(){
-	return gulp.src([config.buildDir+'/css/*.css'],{base: config.buildDir})
-		.pipe(gulp.dest(config.distDir))
-		.pipe(rev())
-		.pipe(gulp.dest(config.distDir))
-		.pipe(rev.manifest({
-			merge: true
-		}))
-		.pipe(gulp.dest(config.distDir))
+    return gulp.src([config.buildDir+'/css/*.css'],{base: config.buildDir})
+        .pipe(clean())
+        .pipe(gulp.dest(config.distDir))
+        .pipe(rev())
+        .pipe(gulp.dest(config.distDir))
+        .pipe(rev.manifest({
+            merge: true
+        }))
+        .pipe(gulp.dest(config.distDir))
 });
 
-gulp.task('build', ['clean:css', 'rev', 'swig:build', 'copy:js']);
-gulp.task('default', ['watch', 'sass', 'swig']);
+gulp.task('copy:js', function(){
+    return gulp.src(`${config.buildDir}/js/*.*`)
+        .pipe(gulp.dest(`${config.distDir}/js`))
+});
 
-// // for java project
-// gulp.task('default', ['watch', 'sass', 'swig', 'generateDevManifest']);
-// gulp.task('build', ()=>{
-// 	runSequence('swig:build', 'rev', 'copy:js', 'generateProdManifest')
-// });
+gulp.task('build', ()=>{
+    runSequence('rev', 'copy:js')
+});
+
+//'generateDevManifest'
+gulp.task('default', ['watch', 'sass']);
